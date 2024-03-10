eff4dc052d7f5df20a8a4d9b46766c9681c666a0
Artemy <artegoser@gmail.com>
2024-03-10 22:12:42 +0300

    doc: add docs site

---
+++ packages/docs/theme.config.jsx
@@ -0,0 +1,8 @@
+export default {
+  logo: <span>OFlePS</span>,
+  docsRepositoryBase:
+    "https://github.com/artegoser/OFlePS/tree/main/packages/docs",
+  project: {
+    link: "https://github.com/artegoser/OFlePS",
+  },
+};
