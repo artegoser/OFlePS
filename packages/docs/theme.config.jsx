633e1f004be67f008526439fda2d4dd7472c18e8
Artemy <artegoser@gmail.com>
2024-03-11 13:10:28 +0300

    doc: db architecture

--- packages/docs/theme.config.jsx
+++ packages/docs/theme.config.jsx
@@ -1,5 +1,10 @@
 export default {
-  logo: <span>OFlePS</span>,
+  logo: (
+    <img
+      style={{ width: "5rem" }}
+      src="https://raw.githubusercontent.com/artegoser/OFlePS/main/imgs/logo.png"
+    />
+  ),
   docsRepositoryBase:
     "https://github.com/artegoser/OFlePS/tree/main/packages/docs",
   project: {
